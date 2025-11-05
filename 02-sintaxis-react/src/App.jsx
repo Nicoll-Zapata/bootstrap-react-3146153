cea1a581c576ea89fa03d9b172474b89dc8e528c
Nicoll-Zapata <zapatanicolmeliza@gmail.com>
2025-11-05 14:07:33 -0500

    Tarjetas

--- 02-sintaxis-react/src/App.jsx
+++ 02-sintaxis-react/src/App.jsx
@@ -1,12 +1,63 @@
 import React from 'react'
 import "./App.css"
 import { NavBar } from './components/NavBar'
+import { ExtensionCard } from './components/ExtensionCard'
 
 export const App = () => {
   return (
     <>
     <NavBar/>
+    <div className='containerCards'> 
+    <ExtensionCard 
+     cardTitle=""
+     cardImage=""
+     cardDescription=""
+    />
+    <ExtensionCard 
+     cardTitle=""
+     cardImage=""
+     cardDescription=""
+    />
+    <ExtensionCard 
+     cardTitle=""
+     cardImage=""
+     cardDescription=""
+    />
+    <ExtensionCard 
+     cardTitle=""
+     cardImage=""
+     cardDescription=""
+    />
+    <ExtensionCard 
+     cardTitle=""
+     cardImage=""
+     cardDescription=""
+    />
+    <ExtensionCard 
+     cardTitle=""
+     cardImage=""
+     cardDescription=""
+    />
+    <ExtensionCard 
+     cardTitle=""
+     cardImage=""
+     cardDescription=""
+    />
+    <ExtensionCard 
+     cardTitle=""
+     cardImage=""
+     cardDescription=""
+    />
+    <ExtensionCard 
+     cardTitle=""
+     cardImage=""
+     cardDescription=""
+    />
+    
+    </div>
+
     </>
 
   )
 }
+
--- 02-sintaxis-react/src/components/ExtensionCard.jsx
+++ 02-sintaxis-react/src/components/ExtensionCard.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import "../stylesheets/ExtensionCard.css"
 
-export const ExtensionCard = () => {
+export const ExtensionCard = ({cardTitle, cardImage, cardDescription}) => {
   return (
-    <div>ExtensionCard</div>
+    <div className='general'>
+      <div className='informacion'>
+        <div className='icon'>
+        <img className='icono' src="" alt="" />
+
+        </div>
+        <div className='texto'>
+          <h1 className='titulo'>DevLes</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iusto dolorum quo explicabo </p>
+        </div>
+      </div>
+      <div className='remove'>
+        <button className='remove1'>Remove</button>
+        <div className='boton'>⏺</div>
+      </div>
+    </div>
   )
 }
