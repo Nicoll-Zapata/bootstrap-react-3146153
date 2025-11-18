75623d8d7212e2137cf4c6a6e8ec9458d013924d
Nicoll-Zapata <zapatanicolmeliza@gmail.com>
2025-11-18 16:03:48 -0500

    Metrika-avance

--- 03-metrika/src/components/NavB.jsx
+++ 03-metrika/src/components/NavB.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+
 
 export const NavB = () => {
   return (
-    <div>
-        <div className='Nav'>
+    <div className="Titulo">
+        <div className="bg-secondary m-auto mt-3" style={{width: "80px", height: "80px"}}>
             <h2 className='titulo1'>Metrika</h2>
         </div>
-        <button></button>
+        <div className="bi bi-facebook"></div>
+        <div className="bi bi-twitter"></div>
+        <div className="bi bi-google"></div>
+        <div className="bi bi-snapchat"></div>
+        <div className="bi bi-whatsapp"></div>
+
+
+
     </div>
   )
 }
