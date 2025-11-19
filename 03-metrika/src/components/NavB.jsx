4837b8614af97b096c71c750003a671b6bd6ec54
Nicoll-Zapata <zapatanicolmeliza@gmail.com>
2025-11-19 13:25:49 -0500

    Iconos

--- 03-metrika/src/components/NavB.jsx
+++ 03-metrika/src/components/NavB.jsx
@@ -3,14 +3,18 @@
 export const NavB = () => {
   return (
     <div className="Titulo">
-        <div className="bg-secondary m-auto mt-3" style={{width: "80px", height: "80px"}}>
-            <h2 className='titulo1'>Metrika</h2>
-        </div>
+      <div className="mt-5" style={{ height: "80px" }}>
+        <h2 className='titulo1'>Metrika</h2>
+      </div>
+
+      <div className="d-sm-flex column-gap-3">
         <div className="bi bi-facebook"></div>
         <div className="bi bi-twitter"></div>
         <div className="bi bi-google"></div>
         <div className="bi bi-snapchat"></div>
         <div className="bi bi-whatsapp"></div>
+
+      </div>
 
 
 
