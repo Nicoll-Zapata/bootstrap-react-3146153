d8be6f12a8e662faefca106aebfc3e12269bb5b8
Nicoll-Zapata <zapatanicolmeliza@gmail.com>
2025-12-14 21:37:05 -0500

    Visumbrio

--- 04-Visumbrio/src/App.jsx
+++ 04-Visumbrio/src/App.jsx
@@ -1,38 +1,15 @@
-import "../src/stylesheets/Header.css"
 import { Desafios } from "./components/Desafios"
 import { Footer } from "./components/Footer"
 import { Inicio } from "./components/Inicio"
 import { Justificacion } from "./components/Justificacion"
 import { Tarjetas } from "./components/Tarjetas"
-import {InicioTienda} from "./components/InicioTienda"
 import { Productos } from "./components/Productos"
-import {Pago} from "./components/Pago"
+import { Header } from "./components/Header"
 
 export const App = () => {
   return (
     <>
-    <header>
-
-     <div className="header">
-
-      <div className="text-center pt-2">
-        <img 
-          src="./src/assets/Logotipo (1).svg"   
-          alt="Visumbrío Logo"
-          className="logo-img"
-        />
-      </div>
-
-      <nav className="nav justify-content-center mt-3 menu-principal">
-        <a className="nav-link" href="#">Inicio</a>
-        <a className="nav-link" href="#">Cómic</a>
-        <a className="nav-link" href="#">Integrantes</a>
-        <a className="nav-link" href="#">Tienda</a>
-      </nav>
-
-    </div>
-
-    </header>
+    <Header/>
 
     <Inicio/>
 
@@ -44,9 +21,11 @@ export const App = () => {
 
     <Footer/>
 
-    <InicioTienda/>
+    <Header/>
 
     <Productos/>
+
+    <Footer/>
 
     
     </>
--- 04-Visumbrio/src/components/Productos.jsx
+++ 04-Visumbrio/src/components/Productos.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import "../stylesheets/Productos.css"
 
 export const Productos = () => {
   return (
-    <div>Productos</div>
+    <div className="container flex-grow-1">
+
+        <h3 className='text-center mb-5 tituli'>TIENDA</h3>
+        <h5 className="text-center mb-5">NUESTROS PRODUCTOS</h5>
+
+        <div className="row g-5 justify-content-center">
+          <div className="col-6 col-md-4 col-lg-3 text-center">
+            <img src="./src/assets/Pantalon.png" className="img-fluid mb-2 product-img" alt="Pantalón" />
+            <p className="nombre">Pantalón Negro</p>
+          </div>
+
+          <div className="col-6 col-md-4 col-lg-3 text-center">
+            <img src="./src/assets/billetera.png" className="img-fluid mb-2 product-img" alt="Billetera" />
+            <p className="nombre">Billetera Visumbrio</p>
+          </div>
+
+          <div className="col-6 col-md-4 col-lg-3 text-center">
+            <img src="./src/assets/medias.png" className="img-fluid mb-2 product-img" alt="Medias" />
+            <p className="nombre">Medias Negras</p>
+          </div>
+
+          <div className="col-6 col-md-4 col-lg-3 text-center">
+            <img src="./src/assets/gorro-blanco.png" className="img-fluid mb-2 product-img" alt="Gorro" />
+            <p className="nombre">Gorro Blanco</p>
+          </div>
+
+          <div className="col-6 col-md-4 col-lg-3 text-center">
+            <img src="./src/assets/botella.png" className="img-fluid mb-2 product-img" alt="Botella" />
+            <p className="nombre">Botella Negra</p>
+          </div>
+        </div>
+      </div>
   )
 }
